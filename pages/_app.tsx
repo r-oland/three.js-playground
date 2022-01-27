8d1b53f586209207ac26b5603e35d5f129b5bc8f
Roland Branten <roland.branten@gmail.com>
2022-01-27 16:18:55 +0100

    hot border box fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import React from 'react';
 import dynamic from 'next/dynamic';
+import '../styles/hotfix.css';
 
 // disable ssr for react suspense API
 const Layout = dynamic(() => import('../Layout/Layout'), { ssr: false });
